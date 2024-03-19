030ecd9519ac9ef03ca5d3a0e844829e94a321dc
lijo-antony <lijo.a@cormsquare.com>
2024-03-19 22:49:54 +0530

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using BookStoreWebApp.Repository;
+using BookStoreWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public List<BookViewModel> AllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookViewModel Book(int id)
+        {
+            return _bookRepository.GetById(id);
+        }
+        public BookViewModel BookDetail(string author)
+        {
+            return _bookRepository.GetByName(author);
+        }
+    }
+}
---
+++ Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookStoreWebApp.ViewModels;
+
+namespace BookStoreWebApp.Repository
+{
+    public class BookRepository
+    {
+        public List<BookViewModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookViewModel GetById(int id)
+        {
+            return DataSource().FirstOrDefault(b => b.Id == id);
+        }
+
+        public BookViewModel GetByName(string author)
+        {
+            return DataSource().FirstOrDefault(b => b.Author == author);
+        }
+
+        private List<BookViewModel> DataSource()
+        {
+            return
+            [
+                new BookViewModel() {Id = 1, Title = "Kayar", Author = "Thakazhi Sivasankara Pillai"},
+                new BookViewModel() {Id = 2, Title = "Naalukettu", Author = "M. T. Vasudevan Nair"},
+                new BookViewModel() {Id = 3, Title = "Chemmeen", Author = "Thakazhi Sivasankara Pillai"},
+                new BookViewModel() {Id = 4, Title = "Balyakalasakhi", Author = "Vaikom Muhammad Basheer"},
+                new BookViewModel() {Id = 5, Title = "Aadujeevitham", Author = "Benyamin"}
+            ];
+
+        }
+    }
+}
