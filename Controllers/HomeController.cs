2bb478a449cd135e2ce05af7a624337148666c97
lijoantony7 <lijoa1997@gmail.com>
2024-03-24 08:06:36 +0530

    layout configs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,8 +12,13 @@ namespace BookStoreWebApp.Controllers
         public ViewResult About() 
         {
             //View Discovery takes places to find the view
-            //View Discovery ditermine which file to be opened.
+            //View Discovery determine which file to be opened.
             //will search for the VIEW in view->particular folder and view->shared folder.
+            return View();
+        }
+
+        public ViewResult ContactUs()
+        {
             return View();
         }
     }
