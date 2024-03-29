a75d703a21cf667194094790b1da353c931842da
lijoantony7 <lijoa1997@gmail.com>
2024-03-29 08:22:37 +0530

    added form using tagHelpers and more

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -8,26 +8,37 @@ namespace BookStoreWebApp.Controllers
     {
         private readonly BookRepository _bookRepository;
 
-        public BookController()
+        public BookController ()
         {
             _bookRepository = new BookRepository();
         }
 
-        public ViewResult AllBooks()
+        public ViewResult AllBooks ()
         {
             var result = _bookRepository.GetAllBooks();
-            return View(result);
+            return View( result );
         }
 
-        public ViewResult BookDetails(int id)
+        public ViewResult BookDetails ( int id )
         {
-            var result = _bookRepository.GetById(id);
-            return View(result);
+            var result = _bookRepository.GetById( id );
+            return View( result );
         }
 
-        public BookViewModel BookDetail(string author)
+        public ViewResult AddBook ()
         {
-            return _bookRepository.GetByName(author);
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddBook (BookViewModel bookViewModel)
+        {
+            return View();
+        }
+
+        public BookViewModel BookDetail ( string author )
+        {
+            return _bookRepository.GetByName( author );
         }
     }
 }
